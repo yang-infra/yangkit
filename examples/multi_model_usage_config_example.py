682eb839da3f561d66cb817cf21043b67c15987a
Jhansi M N S <jhanm@cisco.com>
2023-08-23 02:06:24 -0700

    Update Example

--- examples/multi_model_usage_config_example.py
+++ examples/multi_model_usage_config_example.py
@@ -21,6 +21,7 @@ ncclient_manager = manager.connect(host=router_info["host"],
                                    hostkey_verify=False,
                                    look_for_keys=False,
                                    allow_agent=False)
+config_list = []
 
 # Set Interface Config
 interface = {"interface_name": "FourHundredGigE0/0/0/0", "address": "1.1.1.1", "netmask": "255.0.0.0"}
@@ -29,28 +30,20 @@ ifcfg.interface_name = interface["interface_name"]
 ifcfg.ipv4.addresses.address = ifcfg.ipv4.addresses.Address()
 ifcfg.ipv4.addresses.address.address = interface["address"]
 ifcfg.ipv4.addresses.address.netmask = interface["netmask"]
-request_payload = Codec.encode(entity=ifcfg, encoding="XML", optype="create")
-print(request_payload)
-config_response = ncclient_manager.edit_config(target="candidate", config=request_payload)
-print(config_response)
-commit_response = ncclient_manager.commit()
-print(commit_response)
+config_list.append(ifcfg)
 
 # Set Segment Routing Config
 srcfg = Cisco_IOS_XR_um_segment_routing_cfg.SegmentRouting.GlobalBlock()
 srcfg.lower_bound = int(20000)
 srcfg.upper_bound = int(21000)
-request_payload = Codec.encode(entity=srcfg, encoding="XML", optype="create")
-print(request_payload)
-config_response = ncclient_manager.edit_config(target="candidate", config=request_payload)
-print(config_response)
-commit_response = ncclient_manager.commit()
-print(commit_response)
+config_list.append(srcfg)
 
 # Set GRPC Config
 grpccfg = Cisco_IOS_XR_um_grpc_cfg.Grpc()
 grpccfg.port = int(57400)
-request_payload = Codec.encode(entity=grpccfg, encoding="XML", optype="create")
+config_list.append(grpccfg)
+
+request_payload = Codec.encode(entity=config_list, encoding="XML", optype="create")
 print(request_payload)
 config_response = ncclient_manager.edit_config(target="candidate", config=request_payload)
 print(config_response)
